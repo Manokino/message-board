c760dd3d54428a108c6aea0c243374de6575a5e0
Saito <shirokuro05suno@gmail.com>
2018-05-25 11:03:34 +0000

    bootstrap design error

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,58 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  
+	def index
+		@messages = Message.order(created_at: :desc).page(params[:page]).per(3)
+	end
+
+	def show
+	end
+
+	def new
+		@messages = Message.new
+	end
+
+	def create
+		@message = Message.new(message_params)
+
+    if @message.save
+      flash[:success] = 'Message が正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message が投稿されませんでした'
+      render :new
+    end
+	end
+
+	def edit
+	end
+
+	def update
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
+    end
+	end
+
+	def destory
+    @message.destroy
+
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to messages_url
+	end	
+
+  private
+
+  # Strong Parameter
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
+  def message_params
+    params.require(:message).permit(:content, :title)
+  end
+  
+end
